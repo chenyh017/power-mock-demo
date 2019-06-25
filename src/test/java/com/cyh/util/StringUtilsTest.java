bfc134c12f466961e511d639bff9a4895929da3a
chenyehai <309848183@qq.com>
2019-06-25 16:50:15 +0800

    mock jdbc

--- src/test/java/com/cyh/util/StringUtilsTest.java
+++ src/test/java/com/cyh/util/StringUtilsTest.java
@@ -1,8 +1,12 @@
 package com.cyh.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
@@ -26,11 +30,24 @@ public class StringUtilsTest {
 
 	@Mock
 	private JdbcTemplate jdbcTemplate;
+	
+	@InjectMocks
+	private JdbcWrapper wrapper;
 
 	@Test
 	public void jdbc() {
-		Mockito.when(jdbcTemplate.queryForObject("sql", Long.class)).thenReturn(2l);
-		System.out.println(jdbcTemplate.queryForObject("sql", Long.class));
+		Mockito.when(jdbcTemplate.queryForObject("sql", String.class)).thenReturn("123456");
+		System.out.println(wrapper.query());
+		
+		List<String> list = new ArrayList<>();
+		list.add("123");
+		list.add("12356");
+		Mockito.when(jdbcTemplate.query("sql",JdbcWrapper.FUNC)).thenReturn(list);
+		System.out.println(wrapper.queryByRowMapper());
+		
+		Mockito.when(jdbcTemplate.queryForList(Mockito.anyString())).thenReturn(null);
+		System.out.println(wrapper.queryForList());
+		
 	}
 
 }
